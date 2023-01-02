a59bc1ab37f06aa2b8cee02321616d75b2d51bf7
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-02 17:10:50 -0500

    added sunset and rise parameters api call data

--- ISS/main.py
+++ ISS/main.py
@@ -1,12 +1,18 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude, latitude)
-print(iss_position)
+print(data)
