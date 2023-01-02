a5d134c42fea2ccf034df24a1a0415f05adffbd0
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-02 16:35:02 -0500

    created longi and lati for iss positions

--- ISS/main.py
+++ ISS/main.py
@@ -5,4 +5,8 @@ response.raise_for_status()
 
 data = response.json()
 
-print(data)
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
