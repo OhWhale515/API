341bb9777bdbf952884510948cfcc1e081c8e8c5
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-05 12:16:43 -0500

    added splits to rise and set

--- ISS/main.py
+++ ISS/main.py
@@ -24,7 +24,12 @@ parameters = {
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-print(sunrise.split)
+print(sunrise)
+print(sunrise.split("T")[1].split(":"))
+
+time_now = datetime.now()
+
+print(time_now.hour)
