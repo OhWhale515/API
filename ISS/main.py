c5c69e34aa2e4ae479708c9324ded7a7c5c34376
OhWhale515 <sterlinggreen515@gmail.com>
2023-01-05 14:03:15 -0500

    imported smtplib for email

--- ISS/main.py
+++ ISS/main.py
@@ -1,5 +1,9 @@
 import requests
-from datetime import datetime
+from datetime import 
+import smtplib
+
+MY_EMAIL = "Email Address Here"
+MY_PASSWORD = "Email Password Here"
 
 MY_LAT = 51.507351
 MY_LONG = -0.127758
@@ -17,23 +21,20 @@ def is_iss_overhead():
 # iss_position = (longitude, latitude)
 
 # print(iss_position)
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,     
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True 
     
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-print(sunrise)
-print(sunset)
-
-time_now = datetime.now()
-
-print(time_now.hour)
